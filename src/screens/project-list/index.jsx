efd6176bf03c63741ed9ba70bc24ac056d43fd83
superRongs <rzrfreedom@163.com>
2021-05-30 01:45:19 +0800

    feat: 添加两个自定义hook useMount和useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,13 +1,12 @@
 import * as qs from 'qs'
 import { useEffect, useState } from 'react'
-import { clearnObject } from 'utils/index'
+import { clearnObject, useDebounce, useMount } from 'utils/index'
 import { List } from './list.jsx'
 import { SearchPanel } from './search-panel'
 
 const apiUrl = process.env.REACT_APP_BASE_URL
 
 export const ProjectListScreen = () => {
-  console.log('apiUrl', apiUrl)
   // 下列列表
   const [users, setUsers] = useState([])
   // 输入框
@@ -16,24 +15,33 @@ export const ProjectListScreen = () => {
     personId: '',
   })
   const [list, setList] = useState([])
+  // 防抖延迟
+  const dobounceParam = useDebounce(param, 2000)
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(clearnObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json())
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(clearnObject(dobounceParam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json())
       }
-    )
-  }, [param])
+    })
+  }, [dobounceParam])
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   fetch(`${apiUrl}/users`).then(async (res) => {
+  //     if (res.ok) {
+  //       setUsers(await res.json())
+  //     }
+  //   })
+  // }, [])
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json())
       }
     })
-  }, [])
+  })
   return (
     <div>
       <SearchPanel
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 export const isFalsy = (value) => (value === 0 ? false : !value)
 
 // 清楚对象空值
@@ -11,4 +13,36 @@ export const clearnObject = (object) => {
     }
   })
   return result
+}
+
+// 普通debounce
+export const debounce = (fn, delay) => {
+  let timeout
+  return (...param) => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      fn(...param)
+    }, delay)
+  }
+}
+
+//custom hook debounce
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value)
+  useEffect(() => {
+    // 每次在value变化之后，设置一个定时器
+    const timeout = setTimeout(() => setDebounceValue(value), delay)
+    //每次在上一次useEffect处理完后再运行
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+  return debounceValue
+}
+
+//custom hook
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
 }
