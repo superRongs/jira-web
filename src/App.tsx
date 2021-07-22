d2da56620e9d7d6e5bdfcec4d447b5e321d54dfa
superRongs <rzrfreedom@163.com>
2021-07-22 17:19:17 +0800

    feat: 添加ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { AuthenticatedApp } from 'authenticated-app'
+import { ErrorBoundary } from 'components/error-boundary'
+import { FullPageError } from 'components/lib'
 import { useAuth } from 'context/auth-context'
 import { UnauthenticatedApp } from 'unauthenticated-app'
 import './App.css'
@@ -7,7 +9,10 @@ function App() {
   const { user } = useAuth()
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      {/* ErrorBoundary  错误信息弹出 */}
+      <ErrorBoundary fallbackRender={FullPageError}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   )
 }
